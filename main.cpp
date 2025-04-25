78ed4953d50219c9367733c84e41ff5a77b5ecf9
TomoyaKomatsubara <k024g1010@g.neec.ac.jp>
2025-04-25 16:22:16 +0900

    完成系

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Windows.h>
+#include<Time.h>
 
 typedef void (*PFunc)(int*);
                     //受け取るデータ(保持するデータ)
@@ -8,17 +9,27 @@ typedef void (*PFunc)(int*);
 
 //0を丁、1を半として扱い、丁半を当てるゲーム
 
+
+//ダイスの目(x)を引数として受け取り、それを文章で返す関数
+void PrintDiceEyes(int x)
+{
+	printf("ダイスの目は%dだった！\n\n",x);
+}
+
+//丁か半かを表すint型(0が丁1が半)を受け取り、それに応じ答えを返す関数
+//計算自体は関数外で行っている
 void PrintChouHann(int x)
 {
 	if (x == 0)
 	{
-		printf("丁\n");
+		printf("丁\n");//偶数
 	} else
 	{
-		printf("半\n");
+		printf("半\n");//奇数
 	}
 }
 
+//自分が選んだ丁半と、diceからはじき出した丁半(destiny)を比べ、同じなら勝ち、違うなら負けを返す関数
 void PrintWinOr(int x, int y)
 {
 	if (x == y)
@@ -30,11 +41,9 @@ void PrintWinOr(int x, int y)
 	}
 }
 
-void DisplayResult(int* s)
-{
-	printf("%d秒間待ってやる…\n\n",*s);
-}
 
+//ポインタ関数PFunc型の変数pを引数として受け取り、
+//その関数の引数にsecondを入れる　(この場合はDisplayResultのポインタを持つPFunc型の変数pを引数として受け取り、その引数としてsecondを入れる)
 void SetTimeOut(PFunc p, int second)//関数が入っているから、
                                 //PFunc pもここで呼びだせばOK！！！！！
 {
@@ -42,14 +51,31 @@ void SetTimeOut(PFunc p, int second)//関数が入っているから、
 	Sleep(second * 1000);
 }
 
-
+//SetTimeOutで呼びだして、「 秒間待ってやる」と宣言するだけの関数
+void DisplayResult(int* s)
+{
+	printf("%d秒間待ってやる…\n\n", *s);
+}
 
 
 int main()
 {
-	int destiny = -1;
+	int destiny = -1;//丁半の結果
+	int dice = -1;//ダイスの結果
 
-	destiny = rand() % 2;
+	unsigned int currenTime = time(nullptr);
+	srand(currenTime);
+	dice = rand() % 6 + 1;
+
+	//ここでdiceの結果からdestinyを割り出す
+	if (dice % 2 == 0)//２で割り切れるならdestinyは0(丁(偶数))
+	{
+		destiny = 0;
+	}
+	else
+	{
+		destiny = 1;
+	}
 
 	PFunc p;//pという名前の変数
 	  //PFunc型(int型ポインタ)
@@ -75,9 +101,10 @@ int main()
 
 	
 		printf("答え:\n");
+		PrintDiceEyes(dice);
 		PrintChouHann(destiny);
 	
-
+		//勝ちか負けかを描画する関数を呼びだす！
 		PrintWinOr(destiny, selection);
 
 
