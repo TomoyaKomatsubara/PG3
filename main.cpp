57aaf8fa03433ca9f8de04ad672db1a4f9773591
TomoyaKomatsubara <k024g1010@g.neec.ac.jp>
2025-04-25 14:17:35 +0900

    授業資料途中経過保存

--- main.cpp
+++ main.cpp
@@ -1,34 +1,46 @@
 #include<stdio.h>
 
-
-
-//int main()
-//{
-//	
-//
-//	printf("main = %p\n",main);
-//
-//
-//
-//	return 0;
-//
-//}
-
-void PrintHelloWorld()
+int Add(const int a, const int b)//	足し算の関数
 {
-	printf("HelloWorld\n");
+	int result = a + b;
+	return result;
 }
+
+int Subtract(const int a, const int b)
+{
+	int result = a - b;
+	return result;
+}
+
+//void PrintHelloWorld()
+//{
+//	printf("HelloWorld\n");
+//}
 
 int main()
 {
-	void(*pfunc)();
+	/*void(*pfunc)();
 
 	pfunc = PrintHelloWorld;
 	printf("PrintHelloWorldのアドレス = %p\n", PrintHelloWorld);
 	printf("pfuncの内容 = %p\n", *pfunc);
 	pfunc();
 
+	return 0;*/
+
+	int l = 114;
+	int h = 514;
+
+	int (*calc)(int, int);
+
+	calc = Add;
+	printf("%d\n", calc(l, h));
+
+	calc = Subtract;
+	printf("%d\n", calc(l, h));
+
 	return 0;
+
 }
 
 
