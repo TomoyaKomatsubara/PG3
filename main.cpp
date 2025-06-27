8db3cbe06f6afa20845236ac5c8a6502ce58aa9d
TomoyaKomatsubara <k024g1010@g.neec.ac.jp>
2025-06-27 16:44:45 +0900

    完了版

--- main.cpp
+++ main.cpp
@@ -1,20 +1,62 @@
 #include<stdio.h>
-#include"IShape.h"
-#include"Circle.h"
-#include"Rectangle.h"
+#include<list>
+using namespace std;
+
 
 int main(void)
 {
+	//駅名のリスト
+	list <const char*> eki_list{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Sinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
 
-	IShape* ball1 = new Circle(1.0f,2.0f);//型は派生元
-	ball1->Size();
-	ball1->Draw();
+	//最初期の駅順
+	//イテレーターを使い先頭から順に描画する
+	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
+
+	//1971年
+	//iteratorを使い先頭から順に探索、Tabataがあったらその前にNishi-Nipporiを挿入する
+	for (std::list<const char*>::iterator itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = eki_list.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+	}
+
+	printf("\n\n");
+	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
 
 
 
-	IShape* shikaku = new Rectangle(10.0f, 3.0f);
-	shikaku->Size();
-	shikaku->Draw();
+
+	//2020年
+	//iteratorを使い先頭から順に探索、Tamachiがあったらその前にTakanawa Gatawayを挿入する
+	for (std::list<const char*>::iterator itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	{
+		if (*itr == "Tamachi")
+		{
+			itr = eki_list.insert(itr, "Takanawa Gateway");
+			++itr;
+		}
+	}
+
+	printf("\n\n");
+	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
+
+
 
 	return 0;
 }
