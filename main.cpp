0e42694be028537dcf556e3e54128a72d82bbf3a
TomoyaKomatsubara <k024g1010@g.neec.ac.jp>
2025-07-11 15:08:09 +0900

    完成版

--- main.cpp
+++ main.cpp
@@ -1,62 +1,34 @@
-#include<stdio.h>
-#include<list>
-using namespace std;
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<iostream>
+
+//配布資料の配列
+std::vector<std::string> str = { "k022g0108@g.neec.ac.jp", "k022g0045@g.neec.ac.jp",
+"k022g0007@g.neec.ac.jp",
+"k022g0015@g.neec.ac.jp", "k022g0028@g.neec.ac.jp", "k022g0049@g.neec.ac.jp", "k022g0003@g.neec.ac.jp", "k022g0044@g.neec.ac.jp", "k022g0072@g.neec.ac.jp", "k022g0055@g.neec.ac.jp", "k021g1139@g.neec.ac.jp", "k021g1485@g.neec.ac.jp", "k021g1162@g.neec.ac.jp", "k021g1278@g.neec.ac.jp", "k020g1229@g.neec.ac.jp", "k020g1202@g.neec.ac.jp", "k020g1215@g.neec.ac.jp", "k020g1416@g.neec.ac.jp", "k020g1245@g.neec.ac.jp", "k020g1506@g.neec.ac.jp", "k020g1401@g.neec.ac.jp", "k022g0089@g.neec.ac.jp", "k022g0107@g.neec.ac.jp", "k022g0135@g.neec.ac.jp", "k022g0020@g.neec.ac.jp", "k022g0046@g.neec.ac.jp", "k022g0126@g.neec.ac.jp", "k022g0068@g.neec.ac.jp", "k022g1060@g.neec.ac.jp", "k022g0019@g.neec.ac.jp", "k022g0013@g.neec.ac.jp", "k022g0024@g.neec.ac.jp", "k022g0134@g.neec.ac.jp", "k022g0123@g.neec.ac.jp", "k022g0030@g.neec.ac.jp", "k022g0053@g.neec.ac.jp", "k022g0023@g.neec.ac.jp", "k022g0041@g.neec.ac.jp", "k022g0063@g.neec.ac.jp", "k022g0059@g.neec.ac.jp", "k022g0078@g.neec.ac.jp", "k022g0011@g.neec.ac.jp", "k022g0040@g.neec.ac.jp", "k022g0088@g.neec.ac.jp", "k022g0099@g.neec.ac.jp", "k022g0060@g.neec.ac.jp", "k022g0032@g.neec.ac.jp", "k022g0087@g.neec.ac.jp", "k022g0016@g.neec.ac.jp", "k022g0116@g.neec.ac.jp", "k022g0056@g.neec.ac.jp", "k022g0009@g.neec.ac.jp", "k022g0034@g.neec.ac.jp", "k022g0062@g.neec.ac.jp", "k022g0102@g.neec.ac.jp", "k022g0113@g.neec.ac.jp", "k022g0064@g.neec.ac.jp", "k022g0080@g.neec.ac.jp", "k022g0114@g.neec.ac.jp", "k022g0025@g.neec.ac.jp", "k022g0095@g.neec.ac.jp", "k022g0121@g.neec.ac.jp", "k022g0021@g.neec.ac.jp", "k022g0104@g.neec.ac.jp", "k022g0101@g.neec.ac.jp", "k022g0130@g.neec.ac.jp", "k022g0036@g.neec.ac.jp", "k022g0004@g.neec.ac.jp", "k022g0093@g.neec.ac.jp", "k022g0038@g.neec.ac.jp", "k022g0069@g.neec.ac.jp", "k022g0029@g.neec.ac.jp", "k022g0010@g.neec.ac.jp", "k022g0083@g.neec.ac.jp", "k022g0086@g.neec.ac.jp", "k022g0129@g.neec.ac.jp", "k022g0048@g.neec.ac.jp", "k022g0027@g.neec.ac.jp", "k022g0103@g.neec.ac.jp", "k022g0117@g.neec.ac.jp", "k022g0085@g.neec.ac.jp", "k022g0052@g.neec.ac.jp", "k022g0073@g.neec.ac.jp", "k022g0065@g.neec.ac.jp", "k022g0091@g.neec.ac.jp", "k022g0136@g.neec.ac.jp", "k022g0079@g.neec.ac.jp", "k022g0017@g.neec.ac.jp", "k022g0097@g.neec.ac.jp", "k022g0077@g.neec.ac.jp", "k022g0050@g.neec.ac.jp", "k022g0033@g.neec.ac.jp", "k022g0039@g.neec.ac.jp", "k022g0118@g.neec.ac.jp", "k022g0112@g.neec.ac.jp", "k022g0120@g.neec.ac.jp", "k022g0127@g.neec.ac.jp", "k021g1464@g.neec.ac.jp", "k022g0137@g.neec.ac.jp", "k022g0066@g.neec.ac.jp", "k022g0051@g.neec.ac.jp", "k022g0111@g.neec.ac.jp", "k022g0094@g.neec.ac.jp", "k022g0128@g.neec.ac.jp", "k022g0109@g.neec.ac.jp", "k022g0124@g.neec.ac.jp", "k022g0084@g.neec.ac.jp", "k022g0082@g.neec.ac.jp", "k022g0122@g.neec.ac.jp", "k022g0061@g.neec.ac.jp", "k022g0031@g.neec.ac.jp", "k022g0076@g.neec.ac.jp", "k022g0002@g.neec.ac.jp", "k022g0090@g.neec.ac.jp", "k022g0074@g.neec.ac.jp", "k022g0012@g.neec.ac.jp", "k022g0001@g.neec.ac.jp"
+};
+
 
 
 int main(void)
 {
-	//駅名のリスト
-	list <const char*> eki_list{
-		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Sinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
-	};
-
-	//最初期の駅順
-	//イテレーターを使い先頭から順に描画する
-	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	//coutとendlの間に入れることで表示
+	std::cout << "整列前の配列を表示" << std::endl;
+	//forで回して上記と同様にcoutとendlで表示
+	for (uint32_t i = 0; i < str.size(); i++)
 	{
-		printf("%s\n", *itr);
+		std::cout << str[i] << std::endl;
 	}
-
-
-	//1971年
-	//iteratorを使い先頭から順に探索、Tabataがあったらその前にNishi-Nipporiを挿入する
-	for (std::list<const char*>::iterator itr = eki_list.begin(); itr != eki_list.end(); ++itr)
+	// sort関数を使って配列を整列
+	sort(str.begin(), str.end());
+	//以下同様に整列後の配列を表示
+	std::cout << "整列後の配列を表示" << std::endl;
+	for (uint32_t i = 0; i < str.size(); i++)
 	{
-		if (*itr == "Tabata")
-		{
-			itr = eki_list.insert(itr, "Nishi-Nippori");
-			++itr;
-		}
+		std::cout << str[i] << std::endl;
+
 	}
-
-	printf("\n\n");
-	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr)
-	{
-		printf("%s\n", *itr);
-	}
-
-
-
-
-	//2020年
-	//iteratorを使い先頭から順に探索、Tamachiがあったらその前にTakanawa Gatawayを挿入する
-	for (std::list<const char*>::iterator itr = eki_list.begin(); itr != eki_list.end(); ++itr)
-	{
-		if (*itr == "Tamachi")
-		{
-			itr = eki_list.insert(itr, "Takanawa Gateway");
-			++itr;
-		}
-	}
-
-	printf("\n\n");
-	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr)
-	{
-		printf("%s\n", *itr);
-	}
-
-
-
 
 	return 0;
 }
